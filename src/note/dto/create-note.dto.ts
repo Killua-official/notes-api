00ec0d1b2215cf723815b77cf6fc7f7ddcbca17f
Aruzhan Zharkenova <aruzhan.zharkenova.m@gmail.com>
2025-07-05 23:52:30 +0500

    #5 feat: add validation and error handling

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateNoteDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
 }
 

8794a3be06a7a24ee373256b84d2860af6c9a669
Aruzhan Zharkenova <aruzhan.zharkenova.m@gmail.com>
2025-07-05 22:59:20 +0500

    #3 feat: initial project setup with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoteModule } from './note/note.module';
+import { Note } from './note/entities/note.entity';
 
 @Module({
-  imports: [NoteModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Note],
+      synchronize: true,
+    }),
+    NoteModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
